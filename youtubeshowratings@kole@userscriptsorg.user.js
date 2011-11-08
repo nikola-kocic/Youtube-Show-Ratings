bb34f759190c67c134ca6d5a959dc752acc1f4c2
Nikola Kocic <nikolakocic@gmail.com>
2011-11-08 22:01:41 +0100

    Fixed rating not showing for some videos (use requests instead of javascript callback)

--- youtubeshowratings@kole@userscriptsorg.user.js
+++ youtubeshowratings@kole@userscriptsorg.user.js
@@ -3,18 +3,18 @@
 // @description Show video ratings on Youtube
 // @namespace   kole@userscripts.org
 // @homepageURL https://userscripts.org/scripts/show/113918
-// @version     1.2.1
+// @version     1.2.2
 // @updateURL   https://userscripts.org/scripts/source/113918.meta.js
 // @include     http://*youtube.com/*
 // ==/UserScript==
 
 // MAIN SCRIPT ////////////////////////////////////////////////////////////////////////////////////
 
-if(unsafeWindow == window)
+if (unsafeWindow == window)
 {
 	unsafeWindow = (function() {
-		var el = document.createElement('p');
-		el.setAttribute('onclick', 'return window;');
+		var el = document.createElement("p");
+		el.setAttribute("onclick", "return window;");
 		return el.onclick();
 	}())
 }
@@ -31,45 +31,86 @@ if (wm) {
 showBars();
 
 function OnAttrModified (event) {
-	if (typeof event.target.tagName == 'undefined') return;
+	if (typeof event.target.tagName == "undefined") return;
 
-	if(event.target.tagName.toLowerCase() != "li") {
+	if (event.target.tagName.toLowerCase() != "li") {
 		return;
 	}
 	
 	var newBox = event.target.getElementsByClassName("clip")[0];
-	if(!newBox) {
+	if (!newBox) {
 		return;
 	}
-	boxes.push(newBox);
 	
-	// Get all related video ids for each video box
-	boxesIds.push(getBoxId(newBox));
-	appendJS("http://gdata.youtube.com/feeds/api/videos?v=2&max-results=1&safeSearch=none&q=" + boxesIds[boxesIds.length - 1] + "&alt=json-in-script&callback=jsonCallBackVideoLink");
+	showRating(newBox);
 }
 
-function appendJS(strJsFile) {
-	document.body.appendChild(document.createElement('script')).src = strJsFile;
+function showRating(box)
+{
+	boxes.push(box);
+		
+	// Get all related video ids for each video box
+	boxesIds.push(getBoxId(box));
+	var skip = checkIfAlreadyExists(box);
+	if (!skip)
+	{
+		// Send a request to fetch videos info from YouTube's API to the background page
+		callBackVideoLink(box);
+	}
 }
 
 function showBars()
 {
 	// Get all video boxes in which to put the rating bar at the bottom
 	var tempBoxes = document.getElementsByClassName("clip");
-	for (var i = 0; i < tempBoxes.length; i++) {
-		boxes.push(tempBoxes[i]);
-			
-		// Get all related video ids for each video box
-		boxesIds.push(getBoxId(tempBoxes[i]));
-		
-		// Send a request to fetch videos info from YouTube's API to the background page
-		appendJS("http://gdata.youtube.com/feeds/api/videos?v=2&max-results=1&safeSearch=none&q=" + boxesIds[i] + "&alt=json-in-script&callback=jsonCallBackVideoLink");
+	for (var i = 0; i < tempBoxes.length; ++i) {
+		showRating(tempBoxes[i]);
 	}
 }
 
+function checkIfAlreadyExists(video_div)
+{
+	var childDivs = video_div.getElementsByTagName("div");
+	for (var i = 0; i < childDivs.length; ++i)
+	{
+		if (childDivs[i].getAttribute("id") == "ytrp_rating_bar")
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
 // Receive the videos info
-unsafeWindow.jsonCallBackVideoLink = function jsonCallBackVideoLink(root) { // related videos
-	var success = attachAllBars(boxes, root, true);
+function callBackVideoLink(box) { // related videos
+	var id = getBoxId(box);
+	setTimeout(function() {
+		GM_xmlhttpRequest({
+			method: 'GET',
+			url: "http://gdata.youtube.com/feeds/api/videos/" + id + "?v=2",
+			onload: function(response) 
+			{
+				if (response.status == 200) 
+				{
+					response.responseXML = new 	DOMParser().parseFromString(response.responseText, "text/xml");
+					var entry   = response.responseXML.getElementsByTagName("entry"); //root element of response is entry
+					var rating  = entry[0].getElementsByTagName('yt:rating')[0];      // tag name yt:rating holds the ratings
+				} 
+				else
+				{
+					return;
+				}
+				if (rating)
+				{
+					var num_likes    = parseInt(rating.getAttribute("numLikes"));
+					var num_dislikes = parseInt(rating.getAttribute("numDislikes"));
+
+					var i = boxesIds.indexOf(id);
+					attachBar(box, id, num_likes, num_dislikes, true);
+				}
+			}
+		});
+	}, 0);
 }
 
 // HELPER FUNCTIONS ///////////////////////////////////////////////////////////////////////////////
@@ -127,116 +168,66 @@ function getBoxId(box)
 }
 
 
-// Attaches the rating bar to all the video boxes, using the "boxes" nodes and "videoHashtable" info, checking first if any video has it already attached (check = true). Returns true if at least one bar was attached.
-function attachAllBars(boxes, root, check)
-{
-	var success = false;
-	var num_likes = 0;
-	var num_dislikes = 0;
-	var id = "";
-	
-	try
-	{
-		id = root.feed.entry[0].media$group.yt$videoid.$t;
-		num_likes = parseInt(root.feed.entry[0].yt$rating.numLikes);
-		num_dislikes = parseInt(root.feed.entry[0].yt$rating.numDislikes);
-		
-		var i = boxesIds.indexOf(id);
-		if(i >= 0)
-		{
-			attachBar(boxes[i], id, num_likes, num_dislikes, check);
-		}
-	
-		success = true;
-	}
-	catch (err)
-	{
-		id = "";
-		num_likes = 0;
-		num_dislikes = 0;
-	}
-
-	return success;
-}
-
 // Attaches the rating bar to the bottom of the element, checking first if already has id attached (check = true)
 function attachBar(videoThumb, views, likes, dislikes, check) // views not used at the moment, but working
 {
-	var skip = false;
-	if (check)
-	{
-		var childDivs = videoThumb.getElementsByTagName("div");
-		for (var i = 0; i < childDivs.length; i++)
-		{
-			if (childDivs[i].getAttribute("id") == "ytrp_rating_bar")
-			{
-				skip = true;
-				break;
-			}
-		}
-	}
-
-	if (!skip)
-	{
+	var total = likes + dislikes;
+	var totalWidth = videoThumb.offsetWidth;
 	
-		var total = likes + dislikes;
-		var totalWidth = videoThumb.offsetWidth;
+	if (totalWidth > 0)
+	{
+		var likesWidth = 0;
+		if (total > 0) likesWidth = Math.floor((likes / total) * totalWidth);
+		var dislikesWidth = 0;
+		if (total > 0) dislikesWidth = Math.ceil((dislikes / total) * totalWidth);
 		
-		if (totalWidth > 0)
+		// Keep white spacing between bars
+		if (likesWidth > 0 && dislikesWidth > 0)
 		{
-			var likesWidth = 0;
-			if (total > 0) likesWidth = Math.floor((likes / total) * totalWidth);
-			var dislikesWidth = 0;
-			if (total > 0) dislikesWidth = Math.ceil((dislikes / total) * totalWidth);
-			
-			// Keep white spacing between bars
-			if (likesWidth > 0 && dislikesWidth > 0)
+			if (likesWidth >= dislikesWidth)
 			{
-				if (likesWidth >= dislikesWidth)
-				{
-					likesWidth -= 1;
-				}
-				else
-				{
-					dislikesWidth -= 1;
-				}
-			}
-			
-			var ratingDiv = document.createElement("div");
-			ratingDiv.setAttribute("id", "ytrp_rating_bar");
-			ratingDiv.setAttribute("style", "position: absolute; top: 0; left: 0; right: 0; height: 5px; background-color: white;");
-			if (total > 0)
-			{
-				ratingDiv.setAttribute("title", (Math.round((likes / total) * 10000) / 100) + "% likes (" + ts(total) + " ratings)");
+				likesWidth -= 1;
 			}
 			else
 			{
-				ratingDiv.setAttribute("title", "No ratings");
+				dislikesWidth -= 1;
 			}
-
-			if (total > 0)
-			{
-				var likesDiv = document.createElement("div");
-				likesDiv.setAttribute("style", "position: absolute; top: 0; left: 0; height: 4px; width: " + likesWidth + "px; background: #060;");
-				
-				
-				var dislikesDiv = document.createElement("div");
-				dislikesDiv.setAttribute("style", "position: absolute; top: 0; right: 0; height: 4px; width: " + dislikesWidth + "px; background: #C00;");
-
-				ratingDiv.appendChild(likesDiv);
-				ratingDiv.appendChild(dislikesDiv);
-			}
-			else
-			{
-				var noRatingsDiv = document.createElement("div");
-				noRatingsDiv.setAttribute("style", "position: absolute; top: 0; left: 0; right: 0; height: 4px;  background: #BBB;");
-				
-				ratingDiv.appendChild(noRatingsDiv);
-			}
-			
-			videoThumb.style.position = "relative";
-			videoThumb.appendChild(ratingDiv);
 		}
+		
+		var ratingDiv = document.createElement("div");
+		ratingDiv.setAttribute("id", "ytrp_rating_bar");
+		ratingDiv.setAttribute("style", "position: absolute; top: 0; left: 0; right: 0; height: 5px; background-color: white;");
+		if (total > 0)
+		{
+			ratingDiv.setAttribute("title", (Math.round((likes / total) * 10000) / 100) + "% likes (" + ts(total) + " ratings)");
+		}
+		else
+		{
+			ratingDiv.setAttribute("title", "No ratings");
+		}
+
+		if (total > 0)
+		{
+			var likesDiv = document.createElement("div");
+			likesDiv.setAttribute("style", "position: absolute; top: 0; left: 0; height: 4px; width: " + likesWidth + "px; background: #060;");
+			
+			
+			var dislikesDiv = document.createElement("div");
+			dislikesDiv.setAttribute("style", "position: absolute; top: 0; right: 0; height: 4px; width: " + dislikesWidth + "px; background: #C00;");
+
+			ratingDiv.appendChild(likesDiv);
+			ratingDiv.appendChild(dislikesDiv);
+		}
+		else
+		{
+			var noRatingsDiv = document.createElement("div");
+			noRatingsDiv.setAttribute("style", "position: absolute; top: 0; left: 0; right: 0; height: 4px;  background: #BBB;");
+			
+			ratingDiv.appendChild(noRatingsDiv);
+		}
+		
+		videoThumb.style.position = "relative";
+		videoThumb.appendChild(ratingDiv);
 	}
 }
 
